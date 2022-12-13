cb0d2b6f87ace9d4ca9f5093c0a5c0c0758e7270
Jordi Isken <72348117+UBONGFX@users.noreply.github.com>
2022-12-13 15:43:55 +0100

    changed folder strcture

---
+++ src/components/PokemanCard.svelte
@@ -0,0 +1,11 @@
+<script>
+	export let pokeman;
+</script>
+
+<a
+	class="p-6 bg-gray-100 text-grey-800 text-center rounded-md shadow-sm hover:shadow-md flex flex-col items-center"
+	href="/pokemon/{pokeman.id}"
+>
+	<img class="w-40 h-40" src={pokeman.image} alt={pokeman.name} />
+	<h2 class="uppercase text-1xl">{pokeman.id}. {pokeman.name}</h2>
+</a>
